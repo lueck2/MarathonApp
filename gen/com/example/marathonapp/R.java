8e19461a5859d1fd1cdaf2ee876a68bef935f07d
unknown <lueck2@tcnj.edu>
2014-06-07 14:33:37 -0400

    work, dammit!

--- gen/com/example/marathonapp/R.java
+++ gen/com/example/marathonapp/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050047;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1113,6 +1113,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int label=0x7f050047;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int login_email=0x7f05003e;
@@ -1203,12 +1204,16 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_list_nonprofits=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int fragment_list_nonprofits=0x7f03001a;
+        public static final int fragment_main=0x7f03001b;
+        public static final int nonprofit_list_item=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int list_nonprofits=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1253,11 +1258,13 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int hello_world=0x7f0a0015;
         public static final int login_button=0x7f0a0012;
         public static final int login_prompt=0x7f0a0010;
         public static final int login_welcome=0x7f0a000e;
         public static final int register_button=0x7f0a0013;
         public static final int register_prompt=0x7f0a0011;
+        public static final int title_activity_list_nonprofits=0x7f0a0014;
     }
     public static final class style {
         /** 
